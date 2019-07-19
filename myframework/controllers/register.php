5cde1f2360ba4250c111fc895a98eb988b8db3f1
Dennis Manyagi <manyagi.dennis@gmail.com>
2019-07-18 18:55:01 -0700

    modal and errors validation

--- myframework/controllers/register.php
+++ myframework/controllers/register.php
@@ -33,33 +33,64 @@ public function index(){
 }
 
 
+
+
+public function registerConfirmed(){
+
+
+    $data = array();
+    $data["pagename"] = "about";
+    $data["navigation"] = array("home"=>"/welcome", "about"=>"/about", "photos"=>"/photos", "register"=>"/register");
+
+    $this->parent->getView("header", $data);
+    $this->parent->getView("registerConfirmed");
+    $this->parent->getView("footer");
+
+
+}
+
+
+
 public function registerAction(){
+
+
+        $err = array();
+
+
 
         //if blank or empty
         if(empty($_POST["name"]) || $_POST["name"] == ""){
 
-            echo "does not exist or blank";
+            array_push($err, "Name does not exist");
+
+            //echo "does not exist or blank";
 
         }else{
 
             //var_dump(preg_match("/^[a-zA-Z]/", $_POST["name"]));
 
 
+            //check expression of input for validation
             if(!preg_match("/^[a-zA-Z]*$/", $_POST["name"])){
 
-                echo"incorrect input";
+                array_push($err, "Incorrect input");
 
 
-            }else{
-
-                echo "valid input";
             }
 
 
             echo "exists";
         }
 
+        //check error count and send message to url
+       if(count($err)>0){
 
+           header("location:/register?msg=".implode("&", $err));
+       }else{
+
+           header("location:/register/registerConfirmed");
+           echo"everything is good";
+       }
        // var_dump($_POST["name"]);
        // var_dump($_POST["lastname"]);
        // var_dump($_GET);
--- myframework/views/header.php
+++ myframework/views/header.php
@@ -29,6 +29,38 @@
 
 <body>
 
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">ERROR</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p><?echo @$_GET["msg"]?></p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<? if(@$_GET["msg"] || @$GET["msg"] != ""){?>
+
+<script type="text/javascript">setTimeout(function(){$("#myModal").modal()}, 500);</script>
+
+<? } ?>
+
+<!-- Button trigger modal -->
+<!--<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+    Launch Photo Modal
+</button> -->
+
 
 <!-- Navigation -->
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
